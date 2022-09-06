01a9b37ab391068ec50910e031cc259cf171eb2a
Gabriel <biel_borel@hotmail.com>
2022-09-06 09:12:26 -0300

    Added Router component and Global Style component to the root

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Router } from './Router';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 };
