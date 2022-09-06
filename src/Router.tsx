505fd642a0a10b4de9246b9b0e7e7549cf689442
Gabriel <biel_borel@hotmail.com>
2022-09-06 09:23:34 -0300

    Added DefaultLayout to all routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
+import { DefaultLayout } from './components/layouts/default';
 import { Profile } from './pages/Profile';
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<Profile />} />
+      <Route path='/' element={<DefaultLayout />}>
+        <Route path='/' element={<Profile />} />
+      </Route>
     </Routes>
   );
 };
