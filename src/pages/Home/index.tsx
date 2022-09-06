ed82f4ca3f90354830fa5e1fac7dc2c3eff6c51b
Gabriel <biel_borel@hotmail.com>
2022-09-06 11:15:24 -0300

    Changed Profile page to Home Page and added UserProfile component to HomePage

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { HomeContainer } from './styles';
+import { UserProfile } from './UserProfile';
+
+export const Home = () => {
+  return (
+    <HomeContainer>
+      <UserProfile />
+    </HomeContainer>
+  );
+};
